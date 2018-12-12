097f1a69396652b98acdbdded8d05a8c283088ea
keyuquan <9298242382@qq.com>
2018-12-12 15:14:42 +0800

    wordCount

--- streams/src/main/scala/com/cad/data/stream/SocketWindowWordCount.scala
+++ streams/src/main/scala/com/cad/data/stream/SocketWindowWordCount.scala
@@ -6,24 +6,20 @@ import org.apache.flink.streaming.api.windowing.time.Time
 object SocketWindowWordCount {
 
   def main(args: Array[String]): Unit = {
-    var hostname: String = "192.168.88.142"
-    var port: Int = 9909
+    var hostname: String = "master"
+    var port: Int = 9999
 
-    // get the execution environment
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
-
-    // get input data by connecting to the socket
     val text: DataStream[String] = env.socketTextStream(hostname, port, '\n')
 
-    // parse the data, group it, window it, and aggregate the counts
+    text.print()
     val windowCounts: DataStream[WordWithCount] = text
       .flatMap { w => w.split("\\s") }
       .map { w => WordWithCount(w, 1) }
       .keyBy("word")
-      .timeWindow(Time.seconds(2))
+      .timeWindow(Time.seconds(10))
       .sum("count")
 
-    // print the results with a single thread, rather than in parallel
     windowCounts.print().setParallelism(1)
 
     env.execute("Socket Window WordCount")
