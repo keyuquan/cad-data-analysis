17e4c626c36401b6262c16513af8340c2571897a
keyuquan <9298242382@qq.com>
2019-01-07 17:17:17 +0800

    代码整理

--- streams-mq-flink/src/main/java/org/apache/rocketmq/flink/example/RocketMQFlinkExample.java
+++ streams-mq-flink/src/main/java/org/apache/rocketmq/flink/example/RocketMQFlinkExample.java
@@ -58,7 +58,6 @@ public class RocketMQFlinkExample {
                 .process ( new ProcessFunction<Map, Map> () {
                     @Override
                     public void processElement(Map in, Context ctx, Collector<Map> out) throws Exception {
-                        System.out.println ( in.get ( "body" ) );
 
                         HTable hTable = HBaseUtils.getAlarmEventTable ();
                         String body = (String) in.get ( "body" );
